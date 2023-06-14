04d6339f33cf120f017c5babdc9120093425106e
shd101wyy <shd101wyy@gmail.com>
2023-06-14 13:58:30 +0800

    added jq

--- home/packages.nix
+++ home/packages.nix
@@ -3,5 +3,6 @@ let
   packages = with pkgs; [
     sops # Mozilla sops (Secrets OPerationS) is an editor of encrypted files
     age # Modern encryption tool with small explicit keys
+    jq # A lightweight and flexible command-line JSON processor
   ];
 in packages
