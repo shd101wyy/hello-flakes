23782d489cc2f004c18de3b4c06a8c470beb2fd8
shd101wyy <shd101wyy@gmail.com>
2023-08-09 15:40:49 +0800

    installed hub

--- home/packages.nix
+++ home/packages.nix
@@ -4,5 +4,6 @@ let
     sops # Mozilla sops (Secrets OPerationS) is an editor of encrypted files
     age # Modern encryption tool with small explicit keys
     jq # A lightweight and flexible command-line JSON processor
+    hub # Command-line wrapper for git that makes you better at GitHub
   ];
 in packages
