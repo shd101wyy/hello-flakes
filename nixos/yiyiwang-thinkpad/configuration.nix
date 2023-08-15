f2e0a1809390513f5c2b6d1b1a1398bba1b774c3
shd101wyy <shd101wyy@gmail.com>
2023-08-15 18:17:05 +0800

    mount fonts and icons to /usr/share

--- nixos/yiyiwang-thinkpad/configuration.nix
+++ nixos/yiyiwang-thinkpad/configuration.nix
@@ -324,6 +324,25 @@ nur, ... }:
     # lxd = { enable = true; };
   };
 
+  # Bind some directories
+  system.fsPackages = [ pkgs.bindfs ];
+  fileSystems = let
+    mkRoSymBind = path: {
+      device = path;
+      fsType = "fuse.bindfs";
+      options = [ "ro" "resolve-symlinks" "x-gvfs-hide" ];
+    };
+    aggregatedFonts = pkgs.buildEnv {
+      name = "system-fonts";
+      paths = config.fonts.fonts;
+      pathsToLink = [ "/share/fonts" ];
+    };
+  in {
+    # Create an FHS mount to support flatpak host icons/fonts
+    "/usr/share/icons" = mkRoSymBind (config.system.path + "/share/icons");
+    "/usr/share/fonts" = mkRoSymBind (aggregatedFonts + "/share/fonts");
+  };
+
   # Nix settings
   nix.settings = {
     # nixPath = [ "nixpkgs=${nixpkgs}" ];
