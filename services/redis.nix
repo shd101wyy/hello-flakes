909098fd8e9af2c8691bcb1f7f9d6ea52c4dc91a
shd101wyy <shd101wyy@gmail.com>
2023-09-08 16:25:19 +0800

    setup local redis cluster

--- services/redis.nix
+++ services/redis.nix
@@ -3,17 +3,30 @@
 {
   services.redis.package = pkgs.redis;
   services.redis.servers = {
-    "instance1" = {
+    "master" = {
       enable = true;
       port = 6379;
       # requirePassFile = "/home/yiyiwang/.credentials/redis-password";
-      requirePass = "foobared";
+      # requirePass = "foobared";
+      requirePass = null;
+      settings = {
+        appendonly = "yes";
+        "cluster-enabled" = "yes";
+      };
     };
-    "instance2" = {
+    "slave1" = {
       enable = true;
       port = 6380;
       # requirePassFile = "/home/yiyiwang/.credentials/redis-password";
-      requirePass = "foobared";
+      # requirePass = "foobared";
+      requirePass = null;
+      slaveOf = {
+        ip = "0.0.0.0";
+        port = 6379;
+      };
+      settings = {
+        appendonly = "yes";
+      };
     };
   };
 }
