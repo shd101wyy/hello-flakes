71d11384c308ed5d2dcb7fda674e21dc58c96bc8
shd101wyy <shd101wyy@gmail.com>
2025-08-27 20:24:33 +0800

    install scc

--- home/packages.nix
+++ home/packages.nix
@@ -43,6 +43,7 @@ let
     redis
     ruby
     pkgsUnstable.rustc
+    scc # Very fast accurate code counter with complexity calculations and COCOMO estimates written in pure Go
     sops # Mozilla sops (Secrets OPerationS) is an editor of encrypted files
     sqlite
     sshpass
